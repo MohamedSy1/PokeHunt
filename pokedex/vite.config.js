a6b1e2daef4a9f01f5028536aa20924a18517a3d
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-30 14:24:24 -0500

    fixed pages

--- pokedex/vite.config.js
+++ pokedex/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
-  // needed for github pages just put the repo name here
-  base: '/PokeHunt/', 
+  build: {
+    outDir: '..',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        pokedex: 'pokedex.html',
+        // Add more pages as needed
+      }
+    }
+  },
+  base: '/PokeHunt/',
 });
